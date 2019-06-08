e467dae2e8e385ae92f717171714fe82d9f813f4
Andrew LeClerc <andrewleclerc93@gmail.com>
2019-06-08 08:09:53 -0400

    added lottery app

---
+++ js/lottery.js
@@ -0,0 +1,12 @@
+const powerball = document.getElementById('powerball');
+const megaMillions = document.getElementById('megaMillions');
+
+// Add function for generating lottery html with parameters
+
+powerball.addEventListener('click', () => {
+
+});
+
+megaMillions.addEventListener('click', () => {
+
+});
